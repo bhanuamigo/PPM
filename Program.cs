c54ad28b0777fff718c6bb25b04d79ed932421c5
Bhanu Prakash <71817772+bhanuamigo@users.noreply.github.com>
2022-11-28 15:42:32 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class ProlificsProjectManagement
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello Prolifics employee");
+        Console.WriteLine("");
+        Console.Write("Enter 1 for adding project               ");
+        Console.WriteLine("Enter 2 for viewing all projects");
+        Console.WriteLine("");
+        Console.Write("Enter 3 for adding Employee              ");
+        Console.WriteLine("Enter 4 for viewing all Employees");
+        Console.WriteLine("");
+        Console.Write("Enter 5 for adding Role                  ");
+        Console.WriteLine("Enter 6 for viewing all Roles");
+        Console.WriteLine("");
+         Console.WriteLine("Enter 7  for exit");
+    }
+}
+
